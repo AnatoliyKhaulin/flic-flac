1e9ab6be9d5ed863c49b6182dec7ffb37463d780
ascend88 <anatolyhaulin@gmail.com>
2019-06-24 11:57:35 +0300

    tmp fixes

--- src/js/header-mobile.js
+++ src/js/header-mobile.js
@@ -4,9 +4,20 @@ let headerTopContainer = document.querySelector('.main-header__top .container');
 let menuBtn = document.querySelector('.js-burger-btn');
 let	menuBurger = menuBtn.querySelector('.main-header__burger');
 let	topHeaderNavList = document.querySelector('.js-top-header-nav-list');
+let	contactsShowBtn = document.querySelector('.js-contacts-show');
+let	contactsHiddenMobile = document.querySelector('.header-contacts');
+let	headerSocialLinks = document.querySelector('.main-header .social-nav');
+let	headerLogo = document.querySelector('.header-logo');
+let	signForm = document.querySelector('.sign-form');
+
+function insertAfter(elementInstertAfter, parentElement) {
+  return parentElement.parentNode.insertBefore(elementInstertAfter, parentElement.nextSibling);
+}
 
 if (window.innerWidth < 1024) {
   headerBottomLine.appendChild(headerTopLine);
+  topHeaderNavList.prepend(signForm);
+  insertAfter(headerSocialLinks, headerLogo);
 
   menuBtn.addEventListener('click', () => {
     if (menuBurger.classList.contains('active')) {
@@ -16,6 +27,31 @@ if (window.innerWidth < 1024) {
       menuBurger.classList.add('active');
       topHeaderNavList.classList.add('active');
     }
+  });
+
+  contactsShowBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (contactsShowBtn.classList.contains('active')) {
+      contactsShowBtn.classList.remove('active');
+      contactsHiddenMobile.classList.remove('active');
+    } else {
+      contactsShowBtn.classList.add('active');
+      contactsHiddenMobile.classList.add('active');
+    }
+  });
+
+  $(document).ready(function() {
+    var s = $(".header-cart__link");
+    var pos = s.position();
+    $(window).scroll(function() {
+      var windowpos = $(window).scrollTop();
+      if (windowpos >= pos.top) {
+        s.addClass("is-fixed");
+      } else {
+        s.removeClass("is-fixed");
+      }
+    });
   });
 }
 
